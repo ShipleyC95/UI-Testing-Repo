dc4ad41f649c0788164b3f4587f9f87559885891
cShipley95 <christopher.shipley@emishealth.com>
2019-02-04 12:26:58 +0000

    working nightwatch tests

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,29 +1,4 @@
-var SELENIUM_CONFIG = {
-    start_process: true,
-    server_path: './bin/selenium-server-standalone-3.9.0.jar',
-    host: '127.0.0.1',
-    port: 4444
-};
-
-var FIREFOX_CONFIG = {
-    browserName: 'chrome',
-    javascriptEnabled: true,
-    acceptSslCerts: true
-};
-
-var DEFAULT_CONFIG = {
-    launch_url: 'http://localhost',
-    selenium_port: 4444,
-    selenium_host: 'localhost',
-    desiredCapabilities: FIREFOX_CONFIG
-}
-
-var ENVIRONMENTS = {
-    default: DEFAULT_CONFIG
-}
-
-module.exports = {
-    src_folders: ['SeleniumTests'],
-    selenium: SELENIUM_CONFIG,
-    test_settings: ENVIRONMENTS
-}
+module.exports = (function(settings) {
+    settings.test_workers = false;
+    return settings
+})(require('./nightwatch.json'))
